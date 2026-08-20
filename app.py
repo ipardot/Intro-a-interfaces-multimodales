1d013b3d0b1603f6191ba5369fbb5bafab13d72d
Isabella Pardo <ipardot@eafit.edu.co>
2026-08-20 13:52:01 -0500

    Fix syntax error in app.py for radio button check

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ with col1:
 with col2: 
   st.subheader("Esta es la segunda columna") 
   modo = st.radio("Que Modalidad es la principal en tu interfaz", ('Visual', 'auditiva', 'Táctil')) 
-  if modo == "Visual':
+  if modo == 'Visual':
      st.write('La vista es fundamental para tu interfaz') 
   if modo == 'auditiva':
      st.write('La audición es fundamental para tu interfaz') 
