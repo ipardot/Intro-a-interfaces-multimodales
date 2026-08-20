26d82326cf6fb14ca2c8ca54afdb7d97f8833d9a
Isabella Pardo <ipardot@eafit.edu.co>
2026-08-20 13:17:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 import streamlit as st
+from PIL import Image
+
 st.title ("Hola, yo soy ipardot")
+
+st.header ("Mi comienzo con apps")
+st.write ("Me gusta mucho la transmedia")
+image = Image.open ('IMG_1036.jpeg')
+st.image(image, caption = 'Representación mental de yo')
