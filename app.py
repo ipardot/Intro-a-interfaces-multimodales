16ba3ccae8416cc69c232baa278a6f09d0df0acf
Isabella Pardo <ipardot@eafit.edu.co>
2026-08-20 13:53:12 -0500

    Fix syntax error in subheader function call

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.write('El texto escrito es', texto)
 st.subheader ("Ahora usemos 2 Columnas")
 col1, col2 = st. columns(2)
 with col1:
-  st.subheader!("£sta es la primera columna")
+  st.subheader("Esta es la primera columna")
   st.write("las interfaces multimodales mejoran la experiencia de usuario") 
   resp = st. checkbox('Estoy de acuerdo')
   if resp: 
